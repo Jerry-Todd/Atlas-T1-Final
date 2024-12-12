a09f4af50f99d86a10f8e4702c98b56cc13ecfdc
Jerry <jerrytodd2005@gmail.com>
2024-12-12 15:19:11 -0600

    Comment most of my Javascript code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,11 @@
 
-
+// Functions to set parts of picture of the day
 const potd = (image) => document.getElementById('potd-image').setAttribute('src', image)
 const potd_title = (text) => document.getElementById('potd-title').textContent = text
 const potd_description = (text) => document.getElementById('potd-description').textContent = text
-const potd_link = 'https://api.nasa.gov/planetary/apod'
 
+// Load information for picture of the day
+const potd_link = 'https://api.nasa.gov/planetary/apod'
 request(potd_link, (data) => {
     potd(data.hdurl)
     potd_title(data.title)
--- assets/js/mars_rovers.js
+++ assets/js/mars_rovers.js
@@ -1,3 +1,4 @@
+// Get elements for each section and hide all except rover list
 const rover_content = document.getElementById('rover-content')
 const cameras_content = document.getElementById('camera-content')
 const images_content = document.getElementById('image-content')
@@ -5,12 +6,14 @@ rover_content.style.display = 'flex'
 cameras_content.style.display = 'none'
 images_content.style.display = 'none'
 
+// Object containing urls for needed API's
+// I thought i would have needed more than one
 const api_links = {
     roverList: 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
 }
-
 let rovers_list
 
+// Get template element for option button, used by rover and camera lists
 const text_option_template = document.getElementsByClassName('option')[0]
 text_option_template.remove()
 
@@ -19,11 +22,13 @@ const rovers_status = document.getElementById('rovers-status')
 
 const rover_options = document.getElementById('rover-options')
 
+// Request list of rovers
 rovers_status.style.display = 'default'
 request(api_links.roverList, (data) => {
     rovers_status.style.display = 'none'
     rovers_list = data.rovers
     console.log(rovers_list)
+    // Create elements for list
     data.rovers.forEach(rover => {
         let option = text_option_template.cloneNode(true)
         option.textContent = rover.name
@@ -34,6 +39,7 @@ request(api_links.roverList, (data) => {
     });
 })
 
+// Change display styles of different sections
 function viewRovers() {
     cameras_content.style.display = 'none'
     rover_content.style.display = 'flex'
@@ -44,6 +50,7 @@ const cameras_status = document.getElementById('cameras-status')
 
 const camera_options = document.getElementById('camera-options')
 
+// Load list of cameras on specific rover and set display styles
 let current_rover
 function viewCameras(rover_name) {
     current_rover = rover_name
@@ -58,19 +65,21 @@ function viewCameras(rover_name) {
     console.log(rover)
 
     camera_options.innerHTML = ''
+    // Create element for each camera
     rover.cameras.forEach(camera => {
         console.log(camera)
         let option = text_option_template.cloneNode(true)
         option.textContent = camera.full_name
         camera_options.append(option)
+        // Click event to make camera load images
         option.addEventListener('click', () => {
             viewImages(rover.name, camera.name)
         })
     })
 }
 
+// Functionality for back button
 const camera_exit = document.querySelector('#camera-content .back-button')
-
 camera_exit.addEventListener('click', () => { viewRovers() })
 
 // Images code
@@ -78,9 +87,11 @@ const images_status = document.getElementById('images-status')
 
 const images_view = document.getElementById('images')
 
+// Get template element for images
 const image_template = document.getElementsByClassName('image')[0]
 image_template.remove()
 
+// Sets display styles to show section and loads images
 function viewImages(rover_name, camera_name) {
     images_view.innerText = ''
     images_status.style.display = 'flex'
@@ -91,6 +102,12 @@ function viewImages(rover_name, camera_name) {
     loadImages(rover_name, camera_name, 10)
 }
 
+// Recursively load images from camera
+/* 
+    Makes many more requests than the amount of photos 
+    it actually loads because most requests dont even return any photos.
+    To fix this it just makes requests until it has gotten 10 photos
+*/
 function loadImages(rover_name, camera_name, amount, index, photo_count) {
     if (index == undefined) {
         index = 1
@@ -118,6 +135,6 @@ function loadImages(rover_name, camera_name, amount, index, photo_count) {
 
 }
 
+// Functionality for back button
 const image_exit = document.querySelector('#image-content .back-button')
-
 image_exit.addEventListener('click', () => { viewCameras(current_rover) })
